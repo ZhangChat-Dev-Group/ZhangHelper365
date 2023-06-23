8cf0cf2236ade46fbfbba0997e89fc343f692eba
MrZhang365 <Xiao_Zhang_123@outlook.com>
2023-06-23 09:43:40 +0800

    添加exit

---
+++ commands/stop.js
@@ -0,0 +1,23 @@
+class Command{
+	constructor(core) {}
+	get info() {
+		return {
+			name: 'stop',
+			developer: 'ZhangSoft',
+			level: 4,
+			explain: '关闭或取消关闭此机器人',
+			usage: '^stop',
+		}
+	}
+	run(core, bot, user, args, payload){
+		if (!core.exitTask) {
+			core.exitTask = setTimeout(() => process.exit(0), 5000)
+			bot.chat(`我将在5秒后停止运行`)
+		} else {
+			clearTimeout(core.exitTask)
+			bot.chat(`已取消倒计时`)
+		}
+	}
+}
+
+module.exports = Command
