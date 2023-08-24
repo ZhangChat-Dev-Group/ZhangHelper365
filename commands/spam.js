3bff1f97d00e40de88c4646078c738eaa0856f11
MrZhang365 <Xiao_Zhang_123@outlook.com>
2023-08-24 18:14:38 +0800

    修改反刷屏的逻辑

--- commands/spam.js
+++ commands/spam.js
@@ -10,36 +10,79 @@ class Command{
 		}
 	}
 	run(core, bot, user, args, payload){}
-	checkSpam(core, bot, payload) {
-		const user = bot.users.get(payload.nick)
-		if (user.nick === core.config.auth.nick) return payload
+	tooFast(core, user, payload) {
+		let warnings = user.warnings || 0
+		if (user.nick === core.config.auth.nick) return { level: 0, warnings, }
 		if (typeof user.lastChat !== 'number') {
 			user.lastChat = payload.time
-			user.warnings = 0
-			return payload
+			warnings = 0
+			return { level: 0, warnings, }
 		}
 		if (typeof user.pardonTime === 'number' && Date.now() >= user.pardonTime) {
-			user.warnings = 0
+			warnings = 0
 		}
 		var tooFast = false
 		if (payload.time - 500 <= user.lastChat) {
-			user.warnings += 1
+			warnings += 1
 			user.pardonTime = Date.now() + 2 * 60 * 1000
 			tooFast = true
 		}
 		user.lastChat = payload.time
-		if (user.warnings < 6 && user.warnings % 3 === 0 && tooFast) bot.chat(`@${payload.nick} \n# ==**你凭什么凌驾于规则之上？**==`)
+		if (warnings < 6 && warnings % 3 === 0 && tooFast) return { level: 1, warnings, }
 		else if (user.warnings >= 6) {
-			bot.chat(`@${user.nick} \n# ==**你是故意找茬儿，是不是？**==`)
+			warnings = 0
+			return { level: 2, warnings, }
+		}
+		return { level: 0, warnings, }
+	}
+	sameContent(core, user, payload) {
+		let warnings = user.warnings || 0
+		if (user.nick === core.config.auth.nick) return { level: 0, warnings, }
+		if (typeof user.lastContent !== 'string') {
+			user.lastContent = payload.text
+			warnings = 0
+			return { level: 0, warnings, }
+		}
+		if (typeof user.pardonTime === 'number' && Date.now() >= user.pardonTime) {
+			warnings = 0
+		}
+		var tooFast = false
+		if (payload.text === user.lastContent) {
+			warnings += 1
+			user.pardonTime = Date.now() + 2 * 60 * 1000
+			tooFast = true
+		}
+		user.lastContent = payload.text
+		if (warnings < 6 && warnings % 3 === 0 && tooFast) return { level: 1, warnings, }
+		else if (warnings >= 6) {
+			warnings = 0
+			return { level: 2, warnings, }
+		}
+		return { level: 0, warnings, }
+	}
+	checkSpam(core, bot, payload) {
+		const user = bot.users.get(payload.nick)
+		const fastResult = this.tooFast(core, user, payload)
+		const sameResult = this.sameContent(core, user, payload)
+
+		var level = fastResult.level
+		if (sameResult.level > level) level = sameResult.level
+
+		var warnings = fastResult.warnings
+		if (sameResult.warnings > warnings) level = sameResult.warnings
+
+		if (level === 1) {
+			bot.chat(`@${user.nick} \n# ==**你凭什么凌驾于规则之上？**==`)
+			return false
+		} else if (level === 2) {
+			bot.chat(`@${user.nick} \n# ==**你是故意找茬，是不是？**==`)
 			bot.sendJSON({
 				cmd: 'dumb',
 				nick: user.nick,
-				time: 1,
+				time: 1
 			})
-			user.warnings = 0
-		}
-		return payload
-
+			return false
+		} else if (level === 0) return payload
 	}
 	initHooks(bot) {
 		bot.regHook('chat', this.checkSpam.bind(this), 25)
--- main.js
+++
@@ -1,7 +0,0 @@
-const CoreApp = require('./kernel/CoreApp.js')
-
-function initKernel() {
-	const kernel = new CoreApp()
-}
-
-initKernel()
