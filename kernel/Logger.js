e0806d9a416002e7a0a0436f3e8a359a7948fd7a
MrZhang365 <Xiao_Zhang_123@outlook.com>
2023-08-24 18:16:51 +0800

    修改反刷屏的逻辑

---
+++ kernel/Logger.js
@@ -0,0 +1,28 @@
+const colors = require('colors')
+const moment = require('moment')
+const fs = require('fs')
+const path = require('path')
+
+class Logger {
+	constructor(dir) {
+		this.dir = dir || path.resolve(__dirname, '..', 'logs')
+	}
+	log(text) {
+		console.log(text)
+		fs.appendFileSync(path.resolve(this.dir, moment().format('YYYY-MM-DD') + '.log'), text + '\n')
+	}
+	info(text) {
+		const prefix = `[${moment().format('YYYY-MM-DD hh:mm:ss')}] [INFO] `.green
+		this.log(prefix + text.split('\n').join('\n' + prefix))
+	}
+	warn(text) {
+		const prefix = `[${moment().format('YYYY-MM-DD hh:mm:ss')}] [WARN] `.yellow
+		this.log(prefix + text.split('\n').join('\n' + prefix))
+	}
+	error(text) {
+		const prefix = `[${moment().format('YYYY-MM-DD hh:mm:ss')}] [ERROR] `.red
+		this.log(prefix + text.split('\n').join('\n' + prefix))
+	}
+}
+
+module.exports = Logger
