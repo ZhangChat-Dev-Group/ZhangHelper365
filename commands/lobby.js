24f88e327cda0fca4db08a4277bb4d5cf78ad592
MrZhang365 <Xiao_Zhang_123@outlook.com>
2023-09-28 17:07:38 +0800

    取消延时加入lobby

--- commands/lobby.js
+++ commands/lobby.js
@@ -34,66 +34,64 @@ class WebSocketPlus extends WebSocket {
 class Command extends events.EventEmitter {
 	constructor(core) {
 		super()
-		setTimeout(() => {
-			if (core.ws) {
-				clearInterval(core.ws.pingTask)
-				core.ws.removeAllListeners()
-				core.ws.terminate()
-			}
-			core.ws = new WebSocketPlus('wss://hack.chat/chat-ws')
-			core.ws.pingTask = setInterval(() => {
-				if (core.ws.readyState !== 1) return
-				core.ws.ping()
-				core.ws.json({
-					cmd: 'ping'
-				})
-			}, 30000)
-			core.ws.users = []
-			core.ws.on('open', () => {
-				core.ws.join(Math.random().toString(36).substr(2, 8), 'lobby')
+		if (core.ws) {
+			clearInterval(core.ws.pingTask)
+			core.ws.removeAllListeners()
+			core.ws.terminate()
+		}
+		core.ws = new WebSocketPlus('wss://hack.chat/chat-ws')
+		core.ws.pingTask = setInterval(() => {
+			if (core.ws.readyState !== 1) return
+			core.ws.ping()
+			core.ws.json({
+				cmd: 'ping'
 			})
+		}, 30000)
+		core.ws.users = []
+		core.ws.on('open', () => {
+			core.ws.join(Math.random().toString(36).substr(2, 8), 'lobby')
+		})
 			
-			core.ws.on('error', () => {
-				clearInterval(core.ws.pingTask)
-				core.logger.error(`Failed to join ?lobby`)
+		core.ws.on('error', () => {
+			clearInterval(core.ws.pingTask)
+			core.logger.error(`Failed to join ?lobby`)
+			core.ws.ready = false
+			core.client.chat(`无法加入 ?lobby ，请联系超级管理员执行 ^reload 来重新连接`)
+		})
+		core.ws.on('close', () => {
+			clearInterval(core.ws.pingTask)
+			core.logger.error(`Connection to ?lobby disconnected`)
+			core.ws.ready = false
+			core.client.chat(`与 ?lobby 的连接被中断，请联系超级管理员执行 ^reload 来重新连接`)
+		})
+		core.ws.on('message', (data) => {
+			const payload = JSON.parse(data)
+			const cmd = payload.cmd
+
+			if (payload.channel !== 'lobby' && payload.channel !== false) {
+				core.logger.error(`Bot in ?lobby was kicked to ?${payload.channel}`)
 				core.ws.ready = false
-				core.client.chat(`无法加入 ?lobby ，请联系超级管理员执行 ^reload 来重新连接`)
-			})
-			core.ws.on('close', () => {
-				clearInterval(core.ws.pingTask)
-				core.logger.error(`Connection to ?lobby disconnected`)
-				core.ws.ready = false
-				core.client.chat(`与 ?lobby 的连接被中断，请联系超级管理员执行 ^reload 来重新连接`)
-			})
-			core.ws.on('message', (data) => {
-				const payload = JSON.parse(data)
-				const cmd = payload.cmd
+				core.client.chat(`在 ?lobby 里面的Bot被踢到了 ?${payload.channel} ，请联系超级管理员执行 ^reload 来重新连接`)
+			}
 
-				if (payload.channel !== 'lobby' && payload.channel !== false) {
-					core.logger.error(`Bot in ?lobby was kicked to ?${payload.channel}`)
-					core.ws.ready = false
-					core.client.chat(`在 ?lobby 里面的Bot被踢到了 ?${payload.channel} ，请联系超级管理员执行 ^reload 来重新连接`)
-				}
+			if (cmd === 'info') {
+				core.logger.info(`info from ?lobby: ${payload.text}`)
+			}
 
-				if (cmd === 'info') {
-					core.logger.info(`info from ?lobby: ${payload.text}`)
-				}
-
-				if (cmd === 'onlineSet') {
-					core.logger.info(`Joined ?lobby`)
-					core.ws.ready = true
-					core.ws.users = payload.nicks
-				} else if (cmd === 'info' && payload.type === 'whisper' && payload.from === 'ultra_weeb' && payload.trip === 'Rdais/') {
-					core.ws.emit('img-res', payload.text.split('\n')[1])
-				} else if (cmd === 'onlineAdd') {
-					core.ws.users.push(payload.nick)
-				} else if (cmd === 'onlineRemove') {
-					core.ws.users = core.ws.users.filter(n => n !== payload.nick)
-				} else if (cmd === 'warn') {
-					core.logger.warn(`?lobby WARNING: ${payload.text}`)
-				}
-			})
-		}, 5000)
+			if (cmd === 'onlineSet') {
+				core.logger.info(`Joined ?lobby`)
+				core.ws.ready = true
+				core.ws.users = payload.nicks
+			} else if (cmd === 'info' && payload.type === 'whisper' && payload.from === 'ultra_weeb' && payload.trip === 'Rdais/') {
+				core.ws.emit('img-res', payload.text.split('\n')[1])
+			} else if (cmd === 'onlineAdd') {
+				core.ws.users.push(payload.nick)
+			} else if (cmd === 'onlineRemove') {
+				core.ws.users = core.ws.users.filter(n => n !== payload.nick)
+			} else if (cmd === 'warn') {
+				core.logger.warn(`?lobby WARNING: ${payload.text}`)
+			}
+		})
 	}
 	get info() {
 		return {
